4e9042ce718216f5bc2667d42aad630cfd8265bb
AstroX809 <87967266+AstroX809@users.noreply.github.com>
2023-09-03 17:19:21 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, IntentsBitField, MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+  ],
+});
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.login(process.env.TOKEN);
+
