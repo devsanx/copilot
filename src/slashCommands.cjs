86eca2ed057302a3638e591fd0e4f050509ea9fb
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-10-26 23:32:04 +0200

    Update slashCommands.cjs

--- src/slashCommands.cjs
+++ src/slashCommands.cjs
@@ -36,8 +36,7 @@ const rest = new REST({ version: "10" }).setToken(`${process.env.TOKEN}`);
 
     await rest.put(
       Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
+        process.env.GUILD_ID // Use only GUILD_ID
       ),
       { body: commands }
     );
